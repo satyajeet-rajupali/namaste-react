6ebe4b420f209df3a91db6f9cbeba7868c3ed2a0
satyajeet <satyajeetrajupali@gmail.com>
2024-02-06 23:56:41 +0530

    Ignited

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-/**
- * 
- * <div id="parent">
- *      <div id="child1">
- *          <h1 id="heading1">Heading h1 from child1</h1>
- *          <h2 id="heading2">Heading h from child1</h2>
- *      </div>
- *      <div id="child2">
- *          <h1 id="heading3">Heading h1 from child1</h1>
- *          <h2 id="heading4">Heading h from child1</h2>
- *      </div> 
- * </div>
- * 
- * 
- * **/
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, [
     React.createElement("div", {id:"child1"}, [
