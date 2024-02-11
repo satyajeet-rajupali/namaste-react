9b98b6cdcb4cbf523725a4ebe80bb211a1c53b38
satyajeet <satyajeetrajupali@gmail.com>
2024-02-11 23:52:42 +0530

    Added Shimmer, Search and API call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,73 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { swiggy_api_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
+
 
 const Body = () => {
 
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredListOfRestaurants, setFilteredListOfRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
-    return (
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+
+        try {
+            const response = await fetch(swiggy_api_URL);
+            const json = await response.json();
+
+            // initialize checkJsonData() function to check Swiggy Restaurant data
+            async function checkJsonData(jsonData) {
+                for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+
+                    // initialize checkData for Swiggy Restaurant data
+                    let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+                    // if checkData is not undefined then return it
+                    if (checkData !== undefined) {
+                        return checkData;
+                    }
+                }
+            }
+
+            // call the checkJsonData() function which return Swiggy Restaurant data
+            const resData = await checkJsonData(json);
+
+            console.log(resData);
+
+            // update the state variable restaurants with Swiggy API data
+            setListOfRestaurants(resData);
+            setFilteredListOfRestaurants(resData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // Conditional Rendering
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }} />
+                    <button className="search-btn" onClick={() => {
+                        const filteredList = listOfRestaurants.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                        setFilteredListOfRestaurants(filteredList);
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn" onClick={() => {
-                    const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
-                    setListOfRestaurants(filteredList);
+                    const filteredList = filteredListOfRestaurants.filter((res) => res.info.avgRating > 4);
+                    setFilteredListOfRestaurants(filteredList);
                 }}>Top Rated Restaurant</button>
             </div>
 
             <div className="res-container">
-                {listOfRestaurants.map(restaurant => (
+                {filteredListOfRestaurants.map(restaurant => (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 ))}
             </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2";
+
+// Swiggy API to get Restaurant data with foodfire erver
+export const swiggy_api_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4271255&lng=78.3271668&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
